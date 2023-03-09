c2b174432908b0872fd4f07cd6634bb0a976c1c7
natnaelbiki <natnaelbiki@gmail.com>
2023-03-09 02:58:49 -0800

    100 calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,4 +16,9 @@ if __name__ == "__main__":
         b = int(sys.argv[3])
         if sys.argv[2] is "+":
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-            pr
+        elif sys.argv[2] is "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif sys.argv[2] is "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif sys.argv[2] is "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
